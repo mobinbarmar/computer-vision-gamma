b9f05a1bb260592835c6e02f87f5f518fb8360cd
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-16 21:21:55 +0330

    end

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ cv2.waitKey(0)
 
 def adjust_gamma(img, gamma = 1.0):
     invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255
+    table = np.array([((i / 255.0) ** gamma) * 255
                      for i in np.arange(0, 256)]).astype('uint8')
     return cv2.LUT(img, table)
 
