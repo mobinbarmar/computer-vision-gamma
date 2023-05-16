f07a853d254de79b3170d483bec4257087efc9f7
Mobin Barmar <barmar.mobin@gmail.com>
2023-05-16 21:15:39 +0330

    done

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('6ZKQoVeY.png', cv2.IMREAD_COLOR)
+gammas = [0.1, 0.2, 0.8, 1.5, 3, 5]
 
 cv2.imshow('Image', img)
 
 cv2.waitKey(0)
+
+
+def adjust_gamma(img, gamma = 1.0):
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+                     for i in np.arange(0, 256)]).astype('uint8')
+    return cv2.LUT(img, table)
+
+
+for gamma in gammas:
+    img = adjust_gamma(img, gamma)
+    cv2.imshow(f'Image with gamma {gamma}', img)
+    cv2.waitKey(0)
